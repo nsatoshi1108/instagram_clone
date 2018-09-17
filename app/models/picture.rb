99d492b89d5076cdc58a3c45db23c42fbb2511f1
satoshi <consultation.pharmcist2@gmail.com>
2018-09-17 19:03:10 +0900

    辞書機能登録

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,8 +1,13 @@
 class Picture < ApplicationRecord
+  validates :img, presence: true
+  validates :title, presence: true,length: {maximum: 30}
+  validates :comment, presence: true,length: {maximum: 255}
+
+
   belongs_to :user
 
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
-  
+
   mount_uploader :img, ImgUploader
 end
