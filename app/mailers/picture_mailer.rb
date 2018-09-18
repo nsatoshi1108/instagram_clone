cb7c19ab9aee1307e83b08682601a8776ed9f0fb
satoshi <consultation.pharmcist2@gmail.com>
2018-09-18 22:05:32 +0900

    CSS・画像修正

--- app/mailers/picture_mailer.rb
+++ app/mailers/picture_mailer.rb
@@ -2,6 +2,6 @@ class PictureMailer < ApplicationMailer
   def picture_mail(picture)
     @picture = picture
     user_info = User.find(@picture.user_id)
-    mail to: "user_info.email", subject: "投稿内容確認メール"
+    mail to: user_info.email, subject: "投稿内容確認メール"
   end
 end
